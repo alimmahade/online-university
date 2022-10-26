d927db61d8e5da8309a79a96e2dddb686f41970f
Your Namealimmahade <alim.mahade@gmail.com>
2022-10-26 23:35:19 +0600

    Courses Pic added

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -15,10 +15,18 @@ const Courses = () => {
             <h1>All courses: {courses.length}</h1>
         <div>
             {
-                courses.map(course=><div>
-                    <p>{course.Name}</p>
-                    <img src={course.picture} alt="" />
-                </div>)
+                courses.map(course=>
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <img className='h-20 w-20' src={course.picture} alt=""/>
+                        <div className="card-body">
+                            <h2 className="card-title">{course.name}</h2>
+                            <p>{course.details}</p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Course Detail</button>
+                            </div>
+                        </div>
+                        </div>
+                )
                     
             }
         </div>
